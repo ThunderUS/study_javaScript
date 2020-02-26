dbced54b1ad0ce6515b24e03be0839f1a08e71c8
Anton <antontokarev.us@gmail.com>
2020-02-25 20:59:46 -0500

    it's first commit

---
+++ HTML/script.js
@@ -0,0 +1,9 @@
+'use strict';
+/*console.log(leftBorderWidh);
+var leftBorderWidh = 1;
+let second = 2;
+const pi = 3.14;
+console.log(second);
+//alert("hello world")*/
+let answer = +prompt("How old are you?", "16");
+console.log(typeof (answer));
